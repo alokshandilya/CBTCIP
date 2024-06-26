52a815b9d9c0983b79050d3a7ae15c0cf33e080f
alokshandilya <alok.shandilya10@gmail.com>
2024-06-26 23:50:10 +0530

    add logic of play_audio

--- voice-record/app.py
+++ voice-record/app.py
@@ -11,3 +11,13 @@ def record_audio(filename, duration, samplerate=44100):
     print("Recording finished.")
     wavio.write(filename, recording, samplerate, sampwidth=2)
     print(f"Audio saved to {filename}")
+
+
+def play_audio(filename):
+    try:
+        wav = wavio.read(filename)
+        sd.play(wav.data, wav.rate)
+        sd.wait()  # Wait until playback is finished
+        print("Playback finished.")
+    except FileNotFoundError:
+        print(f"No file named {filename} found.")
